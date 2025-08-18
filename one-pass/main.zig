ef5d03cdbe63f0b04ab4a83cda5268a3df6f05cd
Nuwan Rajika Kumarasiri <nuwan.r.kumarasiri@gmail.com>
2025-08-18 13:26:22 -0500

    one-pass compiler impl

--- one-pass/main.zig
+++ one-pass/main.zig
@@ -2,7 +2,20 @@
 const std = @import("std");
 
 pub fn main() !void {
-    std.debug.print("one-pass compiler for 4 + 2 * 10 + 3 * (5 + 1)");
+    const program = "4 + 2 * 10 + 3 * (5 + 1)";
+
+    std.debug.print("one-pass compiler for the program: {s}\n", .{program});
+
+    var tokernize = Tokernizer.init(program);
+    while (tokernize.next()) |token| {
+        std.debug.print("{?}: {s}\n", .{ token.tag, program[token.loc.start..token.loc.end] });
+
+        switch (token.tag) {
+            .invalid => return error.TokernizerError,
+            .eoi => break,
+            else => {},
+        }
+    }
 }
 
 const Token = struct {
@@ -15,20 +28,34 @@ const Token = struct {
     };
 
     const Tag = enum {
+        invalid,
+        eoi, // end of input
+
         l_paren,
         r_paren,
+
+        number,
+
         plus,
         mul,
-        invalid,
     };
 
-    fn lexeme(tag: Tag) ?[]const u8 {
+    fn str(tag: Tag) ?[]const u8 {
         return switch (tag) {
-            .invlaid => null,
+            .invlaid, .eoi, .number => null,
             .l_paren => ")",
             .r_paren => "(",
             .plus => "+",
             .mul => "*",
+        };
+    }
+
+    fn symbol(tag: Tag) []const u8 {
+        return tag.str(tag) orelse switch (tag) {
+            .invalid => "invalid token",
+            .eoi => "end of input",
+            .number => "number",
+            else => unreachable,
         };
     }
 };
@@ -38,7 +65,10 @@ const Tokernizer = struct {
     index: usize,
 
     fn init(buffer: [:0]const u8) Tokernizer {
-        return .{.buffer = buffer, index = 0}
+        return .{
+            .buffer = buffer,
+            .index = 0,
+        };
     }
 
     fn print(self: *Tokernizer, token: *const Token) void {
@@ -46,20 +76,83 @@ const Tokernizer = struct {
     }
 
     const State = enum {
-        number, 
-        operator, 
-        paren,
+        start,
+        number,
+        invalid,
     };
 
-    fn next(self:*Tokernizer) ?Token {
-       var result: Token = . {
-        .tag = undefined, 
-        .loc = .{
-            .start = self.index, 
-            .end = undefined,
+    fn next(self: *Tokernizer) ?Token {
+        var result: Token = .{
+            .tag = undefined,
+            .loc = .{
+                .start = self.index,
+                .end = undefined,
             },
-       }; 
+        };
 
-       state: switch(State.start) {}
+        state: switch (State.start) {
+            .start => switch (self.buffer[self.index]) {
+                0 => {
+                    if (self.index == self.buffer.len) {
+                        return .{
+                            .tag = .eoi,
+                            .loc = .{
+                                .start = self.index,
+                                .end = self.index,
+                            },
+                        };
+                    } else {
+                        continue :state .invalid;
+                    }
+                },
+
+                ' ', '\n', '\t', '\r' => {
+                    self.index += 1;
+                    result.loc.start = self.index;
+                    continue :state .start;
+                },
+
+                '0'...'9' => {
+                    result.tag = .number;
+                    self.index += 1;
+                    continue :state .number;
+                },
+
+                '(' => {
+                    result.tag = .l_paren;
+                    self.index += 1;
+                },
+
+                ')' => {
+                    result.tag = .r_paren;
+                    self.index += 1;
+                },
+
+                '+' => {
+                    result.tag = .plus;
+                    self.index += 1;
+                },
+
+                '*' => {
+                    result.tag = .mul;
+                    self.index += 1;
+                },
+                else => result.tag = .invalid,
+            },
+
+            .number => switch (self.buffer[self.index]) {
+                '0'...'9' => {
+                    self.index += 1;
+                    continue :state .number;
+                },
+                else => {},
+            },
+
+            .invalid => {
+                std.debug.print(" tokernizer error => invalid state", .{});
+            },
+        }
+        result.loc.end = self.index;
+        return result;
     }
 };
